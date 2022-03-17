71b954bd590944e5efe3da6c4330343d50d811d4
unknown <nfu40923208>
2022-03-17 16:02:16 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 0808', 'tags': '', 'url': 'About.html'}]};
